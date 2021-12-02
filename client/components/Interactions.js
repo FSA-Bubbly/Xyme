f7dd0736ad2d301251b2fcd88706347ee357ac9e
Sala Yoshida <yoshidasala@gmail.com>
2021-12-02 10:15:17 -0500

    loading icon

--- client/components/Interactions.js
+++ client/components/Interactions.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchInteractions } from '../store/interactions';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchInteractions } from "../store/interactions";
 
 const Interactions = () => {
-  const { auth: user, interactions } = useSelector(s => s);
+  const { auth: user, interactions } = useSelector((s) => s);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -11,42 +11,40 @@ const Interactions = () => {
   }, []);
 
   return (
-    <div>
-      {console.log('component', interactions)}
-      {
-        interactions.length < 1 ? (
+    <div className='flex flex-col'>
+      {console.log("component", interactions)}
+      {interactions.length < 1 ? (
+        <div className=' my-40 self-center text-center'>
+          {" "}
+          <img src='/loading.svg' className=' w-32' />
           <h1>Loading...</h1>
-          ) : (
-          <table>
-            <tr>
-              <th>Medication 1</th>
-              <th>Medication 2</th>
-              <th>Description</th>
+        </div>
+      ) : (
+        <table>
+          <tr>
+            <th>Medication 1</th>
+            <th>Medication 2</th>
+            <th>Description</th>
+          </tr>
+          {interactions.map((interaction) => (
+            <tr key={interaction.id}>
+              <td>
+                {user.pills.map((pill) => {
+                  if (pill.id === interaction.med1Id) return pill.name;
+                })}
+              </td>
+              <td>
+                {user.pills.map((pill) => {
+                  if (pill.id === interaction.med2Id) return pill.name;
+                })}
+              </td>
+              <td>{interaction.interactionDesc}</td>
             </tr>
-            {
-              interactions.map(interaction => (
-                <tr key={interaction.id} >
-                  <td>
-                    {
-                    user.pills.map(pill => {
-                      if (pill.id === interaction.med1Id) return pill.name
-                    })}
-                  </td>
-                  <td>
-                    {
-                    user.pills.map(pill => {
-                      if (pill.id === interaction.med2Id) return pill.name
-                    })}
-                  </td>
-                  <td>{interaction.interactionDesc}</td>
-                </tr>
-              ))
-            }
-          </table>
-        )
-      }
+          ))}
+        </table>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Interactions;
