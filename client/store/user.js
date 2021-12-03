96bcdbaf196a77e397f1c4ed7261c3493f2e21ed
CJ Fung <cjfung94@gmail.com>
2021-12-03 16:13:59 +0000

    Validations

--- client/store/user.js
+++ client/store/user.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import history from '../history';
 import { getToken } from './auth';
-
 const token = getToken();
+
 const UPDATE_USER = 'UPDATE_USER';
 const FETCH_UPDATE_USER = 'FETCH_UPDATE_USER';
 
