83836fc1f2558a9193f64175f49f9f328e4da42b
Sala Yoshida <yoshidasala@gmail.com>
2021-12-11 20:16:25 -0500

    styling for clock

--- client/components/Timer.js
+++ client/components/Timer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 const Timer = (props) => {
   const [time, setTime] = useState(new Date());
@@ -19,49 +19,57 @@ const Timer = (props) => {
 
   return (
     <div>
-      <h1 className="self font-sans uppercase fadeIn p-2 md:text-2xl  text-xl tracking-widest text-center text-gray-800 dark:text-gray-200 text-gray-800 ">
+      <h1 className='rounded-top dark:bg-gray-500 text-center px-5 py-3 bg-nude text-left text-md font-semibold text-gray-600 uppercase tracking-wider dark:text-gray-300'>
         Current Time:
       </h1>
-      <div className="border-8 border-yellow-0 rounded shadow-2x1 font-mono text-9x1  text-white grid grid-cols-3 gap-x-px">
+      <div className=' border-yellow-0 rounded shadow-2x1 font-mono text-9x1  text-white grid grid-cols-3 gap-x-px'>
         {/* left side */}
-        <div className="relative bg-black p-8">
+        <div className='relative bg-black p-8'>
           {/* background grid of black squares */}
-          <div className="absolute inset-0 bg-red-400 grid grid-rows-2">
-            <div className="bg-gradient-to-br from-gray-800 to-gray-800"></div>
-            <div className="bg-gradient-to-br from-gray-700 to-gray-900"></div>
+          <div className='absolute inset-0 bg-red-400 grid grid-rows-2'>
+            <div className='bg-gradient-to-br from-nude to-nude'></div>
+            <div className='bg-gradient-to-br from-nude to-nude'></div>
           </div>
           {/* time numbers */}
-          <span className="relative">{("0" + hours).slice(-2)}</span>
+          <span className='relative text-gray-700 '>
+            {("0" + hours).slice(-2)}
+          </span>
           {/* line across the middle */}
-          <div className="absolute inset-0 flex items-center">
-            <div className="h-px w-full bg-black"></div>
+          <div className='absolute inset-0 flex items-center'>
+            <div className='h-px w-full bg-black'></div>
           </div>
         </div>
         {/* right side */}
-        <div className="relative bg-black p-8">
+        <div className='relative bg-black p-8'>
           {/* background grid of black squares */}
-          <div className="absolute inset-0 bg-red-400 grid grid-rows-2">
-            <div className="bg-gradient-to-br from-gray-800 to-gray-800"></div>
-            <div className="bg-gradient-to-br from-gray-700 to-gray-900"></div>
+          <div className='absolute inset-0 bg-red-400 grid grid-rows-2'>
+            <div className='bg-gradient-to-br from-nude to-nude'></div>
+            <div className='bg-gradient-to-br from-nude to-nude'></div>
           </div>
           {/* time numbers */}
-          <span className="relative"> {("0" + minutes).slice(-2)}</span>
+          <span className='relative text-gray-700'>
+            {" "}
+            {("0" + minutes).slice(-2)}
+          </span>
           {/* line across the middle */}
-          <div className="absolute inset-0 flex items-center">
-            <div className="h-px w-full bg-black"></div>
+          <div className='absolute inset-0 flex items-center'>
+            <div className='h-px w-full bg-black'></div>
           </div>
         </div>
 
-        <div className="relative bg-black p-8">
-          <div className="absolute inset-0 bg-red-400 grid grid-rows-2">
-            <div className="bg-gradient-to-br from-gray-800 to-gray-800"></div>
-            <div className="bg-gradient-to-br from-gray-700 to-gray-900"></div>
+        <div className='relative bg-black p-8'>
+          <div className='absolute inset-0 bg-red-400 grid grid-rows-2'>
+            <div className='bg-gradient-to-br from-nude to-nude'></div>
+            <div className='bg-gradient-to-br from-nude to-nude'></div>
           </div>
           {/* time numbers */}
-          <span className="relative"> {("0" + seconds).slice(-2)}</span>
+          <span className='relative text-gray-700'>
+            {" "}
+            {("0" + seconds).slice(-2)}
+          </span>
           {/* line across the middle */}
-          <div className="absolute inset-0 flex items-center">
-            <div className="h-px w-full bg-black"></div>
+          <div className='absolute inset-0 flex items-center'>
+            <div className='h-px w-full bg-black'></div>
           </div>
         </div>
       </div>
